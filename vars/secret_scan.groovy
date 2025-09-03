0c5b4c43e0efc87982a3aaff7372d5f9d9f24414
Sandeep Singh <contactdevsandeep@gmail.com>
2025-09-04 00:46:09 +0530

    Update secret_scan.groovy

--- vars/secret_scan.groovy
+++ vars/secret_scan.groovy
@@ -8,8 +8,8 @@ def call(Map config = [:]) {
         if (sh(script: "command -v gitleaks >/dev/null 2>&1", returnStatus: true) != 0) {
             echo "📦 Installing Gitleaks..."
             sh '''
-                curl -sSL https://github.com/gitleaks/gitleaks/releases/latest/download/gitleaks-linux-arm64.tar.gz \
-                  | tar -xz -C /usr/local/bin gitleaks
+                curl -sSL https://github.com/gitleaks/gitleaks/releases/latest/download/gitleaks-linux-arm64 \
+                  -o /usr/local/bin gitleaks
                 chmod +x /usr/local/bin/gitleaks
             '''
             echo "📦 Gitleaks installed"
